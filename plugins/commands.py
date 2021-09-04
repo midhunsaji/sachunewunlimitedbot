5afd356e1ba581c55c989f99348df5d8b32aa863
midhunsaji <89266093+midhunsaji@users.noreply.github.com>
2021-09-04 16:49:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Creator", url="https://t.me/Mr_Lucifer_ll"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Channel", url="https://t.me/MoviePlayr"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/MoviePlayr")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/ok_bie_1223/6")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
