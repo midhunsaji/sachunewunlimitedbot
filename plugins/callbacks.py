e561eac6a2eae58e34701dbca8b733a6bcbc6b7b
midhunsaji <89266093+midhunsaji@users.noreply.github.com>
2022-01-31 08:09:05 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Creator", url="https://t.me/Mr_Lucifer_ll"),
+                    InlineKeyboardButton("Creator", url="https://t.me/Curio_VolvEX"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
                     InlineKeyboardButton("Channel", url="https://t.me/MoviePlayr"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/MoviePlayr")
+                    InlineKeyboardButton("Chat Admins", url="https://t.me/talkabout1")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://t.me/ok_bie_1223/6")
+                        "SOURCE CODE", url="https://t.me/ok_bie_1223/21")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
